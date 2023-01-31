e94830e4ae605c387b44f8c74b77a11bf25e6b06
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2023-01-31 13:20:59 +0300

    Links-Functions-Recursion-Passing-arguments

---
+++ pipka/pipka.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Factorial
+{
+public:
+	static int FactorialRecursive(const int &n)
+	{
+		if (n == 0)
+		{
+			return 1;
+		}
+		else
+		{
+			return n * FactorialRecursive(n - 1);
+		}
+	}
+};
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+	setlocale(LC_ALL, "Russian_Russian.1251");
+
+	int n;
+	cout << "Введите число, чтобы вычислить его факториал: " << endl;
+	cin >> n;
+	cout << "Факториал " << n << " равен " << Factorial::FactorialRecursive(n);
+}
